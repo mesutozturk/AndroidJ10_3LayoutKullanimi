54102267346c1c3cc2441d54fa48971b0d58c063
Mesut OZTURK <mesutoztrk@msn.com>
2017-10-11 18:56:38 +0300

    Initial commit

---
+++ app/src/main/java/com/mstztrk/j10_3layoutkullanimi/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mstztrk.j10_3layoutkullanimi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String icerik = "123";
+        if (icerik.matches("^(?=.*\\d).{4,8}$")) {
+            Toast.makeText(this, "Şifre uygundur girebilirsin", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Şifre uygun değil", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
